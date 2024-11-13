8c08ff264b8f695397bd74e8e0fad45806901765
Sebastian Vielma J. <sebastian.vielma03@inacapmail.cl>
2024-11-12 21:10:05 -0300

    Initial commit

---
+++ app/src/main/java/sebastian/vielma/verduritassa/MainActivity.java
@@ -0,0 +1,113 @@
+package sebastian.vielma.verduritassa;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.identity.Identity;
+import com.google.android.gms.auth.api.identity.SignInClient;
+import com.google.android.gms.auth.api.identity.SignInCredential;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+import org.jetbrains.annotations.NotNull;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private SignInClient oneTapClient;
+
+    private EditText email, password;
+    private Button loginButton;
+    private Button googleSignInButton;
+    private TextView registerTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Inicializar los componentes
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        loginButton = findViewById(R.id.loginButton);
+        googleSignInButton = findViewById(R.id.botonGoogleLogin);
+        registerTextView = findViewById(R.id.registerTextView);
+        mAuth = FirebaseAuth.getInstance();
+
+        // Inicio de sesión
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String usuario = email.getText().toString();
+                String contrasena = password.getText().toString();
+
+                if(!validarDatos()) {
+                    Toast.makeText(MainActivity.this, "Por favor, complete todos los campos", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Llamar a firebase
+                    loguearse(usuario, contrasena);
+                }
+            }
+        });
+
+        // Inicio de sesion con google
+        googleSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Google Sign-In Clicked", Toast.LENGTH_SHORT).show();
+                // Aquí puedes implementar la autenticación con Google
+            }
+        });
+
+        // Registro
+        registerTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registro = new Intent(MainActivity.this, RegistroActivity.class);
+                startActivity(registro);
+            }
+        });
+    }
+
+    private void loguearse(String email, String password){
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Intent listaCultivos = new Intent(MainActivity.this, ListaCultivosActivity.class);
+                    startActivity(listaCultivos);
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(MainActivity.this, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private boolean validarDatos(){
+
+        if(email.getText().toString().isEmpty() || password.getText().toString().isEmpty()){
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+
+}
---
+++ app/src/main/java/sebastian/vielma/verduritassa/RegistroActivity.java
@@ -0,0 +1,107 @@
+package sebastian.vielma.verduritassa;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegistroActivity extends AppCompatActivity {
+
+    EditText email, nombre, pais, genero, password;
+    Button registerButton;
+    private FirebaseAuth mAuth;
+    FirebaseFirestore db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_registro);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+        email = findViewById(R.id.email);
+        nombre = findViewById(R.id.nombre);
+        pais = findViewById(R.id.pais);
+        genero = findViewById(R.id.genero);
+        password = findViewById(R.id.password);
+        registerButton = findViewById(R.id.registerButton);
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //rescata valores de los EditText
+                String emailStr = email.getText().toString();
+                String contrasenaStr = password.getText().toString();
+
+                //registra al usuario
+                registrarse(emailStr, contrasenaStr);
+
+            }
+        });
+
+    }
+
+    private void registrarse(String email, String password){
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                String nombreStr = nombre.getText().toString();
+                String paisStr = pais.getText().toString();
+                String generoStr = genero.getText().toString();
+
+                if (task.isSuccessful()) {
+                    //guarda datos del usuario en coleccion usuarios
+                    // Crear un objeto para almacenar datos
+                    Map<String, Object> usuario = new HashMap<>();
+                    usuario.put("nombre", nombreStr);
+                    usuario.put("email", email);
+                    usuario.put("pais", paisStr);
+                    usuario.put("genero", generoStr);
+
+                    // Guardar los datos en Firestore
+                    db.collection("usuarios")
+                            .add(usuario)
+                            .addOnSuccessListener(documentReference -> {
+                                Toast.makeText(getApplicationContext(), "Usuario guardado con éxito", Toast.LENGTH_SHORT).show();
+                            })
+                            .addOnFailureListener(e -> {
+                                Toast.makeText(getApplicationContext(), "Error al guardar usuario", Toast.LENGTH_SHORT).show();
+                            });
+
+                    Intent listaCultivos = new Intent(RegistroActivity.this, ListaCultivosActivity.class);
+                    startActivity(listaCultivos);
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(RegistroActivity.this, "Authentication failed",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+}
